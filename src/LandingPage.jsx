3c631ede7e0cf6321d8e43d247299f6f2051b36f
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-13 11:06:03 +0300

    feat:add image

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,6 +1,9 @@
 import React from "react";
 import logo from "./assets/logo.png";
 import "./LandingPage.css";
+import imgae_1 from "./assets/pexels-danxavier-1239291.jpg";
+import image_2 from "./assets/pexels-olly-733872.jpg";
+import image_3 from "./assets/pexels-stefanstefancik-91227.jpg";
 
 const LandingPage = () => {
   return (
@@ -11,14 +14,20 @@ const LandingPage = () => {
       </nav>
       <main>
         <div className="border o"></div>
-        <div className="border t"></div>
+        <div className="border t">
+          <img src={imgae_1} alt="Image-1" />
+        </div>
         <div className="border th"></div>
         <div className="border f"></div>
         <div className="border fi"></div>
         <div className="border s"></div>
-        <div className="border se"></div>
+        <div className="border se">
+          <img src={image_2} alt="Image-2" />
+        </div>
         <div className="border e"></div>
-        <div className="border n"></div>
+        <div className="border n">
+          <img src={image_3} alt="Image-3" />
+        </div>
         <h1>
           Create a Professional CV in just a <br />
           <span>few Minutes, Absolutely Free!</span>
