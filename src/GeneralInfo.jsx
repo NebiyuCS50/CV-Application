ecdec87a140d48f1621abef4f8c36ce45c01c9b3
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-13 12:04:22 +0300

    feat: add general information

---
+++ src/GeneralInfo.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+const GeneralInfo = ({ formData, handleChange }) => {
+  return (
+    <div>
+      <h2>General Information</h2>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <label>
+        Phone:
+        <input
+          type="tel"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+          required
+        />
+      </label>
+    </div>
+  );
+};
+
+export default GeneralInfo;
