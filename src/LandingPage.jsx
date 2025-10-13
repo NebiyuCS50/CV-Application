8363d9d54852c062872215c0e9a12152485bba6c
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-13 09:09:33 +0300

    feat: add div for styling

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import logo from "./assets/logo.png";
+import "./LandingPage.css";
 
 const LandingPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <nav>
+        <img src={logo} alt="logo" />
+        <p>CVgenZ</p>
+      </nav>
+      <main>
+        <div className="border o"></div>
+        <div className="border t"></div>
+        <div className="border th"></div>
+        <div className="border f"></div>
+        <div className="border fi"></div>
+        <div className="border s"></div>
+        <div className="border se"></div>
+        <div className="border e"></div>
+        <div className="border n"></div>
+        <h1>
+          Create a Professional CV in just a <br />
+          few Minutes, Absolutely Free!
+        </h1>
+      </main>
+    </div>
+  );
 };
 
 export default LandingPage;
