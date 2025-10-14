612654e5b03b96b8d1f0c43e3071599aebe272ca
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-15 01:13:11 +0300

    feat: add experience class name

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,56 +1,72 @@
 import React from "react";
-import "./General.css";
-const Experience = ({ formData, handleChange }) => {
+import "./Experience.css";
+
+const Experience = ({ formData, handleArrayChange, addEntry }) => {
   return (
-    <div className="general-info">
+    <div className="experience-info">
       <h2>Practical Experience</h2>
-      <label>
-        Company Name:
-        <input
-          type="text"
-          name="companyName"
-          value={formData.companyName}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Position Title:
-        <input
-          type="text"
-          name="positionTitle"
-          value={formData.positionTitle}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Main Tasks:
-        <textarea
-          name="mainTasks"
-          value={formData.mainTask}
-          onChange={handleChange}
-          required
-        ></textarea>
-      </label>
-      <label>
-        Start Date:
-        <input
-          type="date"
-          name="dateOfWork"
-          value={formData.startDate}
-          onChange={handleChange}
-          required
-        />
-        End Date:
-        <input
-          type="date"
-          name="dateOfWork"
-          value={formData.endDate}
-          onChange={handleChange}
-          required
-        />
-      </label>
+      {formData.experience.map((exp, index) => (
+        <div key={index}>
+          <input
+            type="text"
+            name="companyName"
+            value={exp.companyName}
+            onChange={(e) => handleArrayChange("experience", index, e)}
+            placeholder="Company Name"
+            required
+          />
+
+          <input
+            type="text"
+            name="positionTitle"
+            value={exp.positionTitle}
+            onChange={(e) => handleArrayChange("experience", index, e)}
+            placeholder="Position Title"
+            required
+          />
+
+          <textarea
+            name="mainTasks"
+            value={exp.mainTasks}
+            onChange={(e) => handleArrayChange("experience", index, e)}
+            placeholder="Main Tasks"
+            required
+          />
+
+          <input
+            type="date"
+            name="startDate"
+            value={exp.startDate}
+            onChange={(e) => handleArrayChange("experience", index, e)}
+            placeholder="Start Date"
+            required
+          />
+
+          <input
+            type="date"
+            name="endDate"
+            value={exp.endDate}
+            onChange={(e) => handleArrayChange("experience", index, e)}
+            placeholder="End Date"
+            required
+          />
+        </div>
+      ))}
+
+      <button
+        type="button"
+        onClick={() =>
+          addEntry("experience", {
+            companyName: "",
+            positionTitle: "",
+            mainTasks: "",
+            startDate: "",
+            endDate: "",
+          })
+        }
+      >
+        + Add Experience
+      </button>
     </div>
   );
 };
