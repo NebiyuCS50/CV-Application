95d5d26c70cd39b7f61323165197d726fa585157
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-15 01:14:16 +0300

    feat: add qualification class name

--- src/QualificationInfo.jsx
+++ src/QualificationInfo.jsx
@@ -1,39 +1,50 @@
 import React from "react";
-import "./General.css";
-const QualificationInfo = ({ formData, handleChange }) => {
+import "./Qualification.css";
+const QualificationInfo = ({ formData, handleArrayChange, addEntry }) => {
   return (
-    <div className="general-info">
+    <div className="qualification-info">
       <h2>Qualifications</h2>
-      <label>
-        Title:
-        <input
-          type="text"
-          name="Title"
-          value={formData.title}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Institution:
-        <input
-          type="text"
-          name="institution"
-          value={formData.institution}
-          onChange={handleChange}
-          required
-        />
-      </label>
-      <label>
-        Year Obtained:
-        <input
-          type="date"
-          name="yearObtained"
-          value={formData.yearObtained}
-          onChange={handleChange}
-          required
-        />
-      </label>
+      {formData.qualifications.map((qual, index) => (
+        <div key={index}>
+          <input
+            name="title"
+            value={qual.title}
+            onChange={(e) => handleArrayChange("qualifications", index, e)}
+            placeholder="Title"
+            required
+          />
+
+          <input
+            type="text"
+            name="institution"
+            value={qual.institution}
+            onChange={(e) => handleArrayChange("qualifications", index, e)}
+            placeholder="Institution"
+            required
+          />
+
+          <input
+            type="date"
+            name="yearObtained"
+            value={qual.yearObtained}
+            onChange={(e) => handleArrayChange("qualifications", index, e)}
+            placeholder="Year Obtained"
+            required
+          />
+        </div>
+      ))}
+      <button
+        type="button"
+        onClick={() =>
+          addEntry("qualifications", {
+            title: "",
+            institution: "",
+            yearObtained: "",
+          })
+        }
+      >
+        + Add Qualification
+      </button>
     </div>
   );
 };
