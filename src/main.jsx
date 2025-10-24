18f26023bb1a4d2d9e2efa8eb034525647e14e25
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-24 21:21:57 +0300

    feat: integrate SpeedInsights component into main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import LandingPage from "./LandingPage";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LandingPage />
     <Analytics />
+    <SpeedInsights />
   </StrictMode>
 );
