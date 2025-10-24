273c1268f9cbc83155bd14b76a53b839524d837d
Nebiyu <nebiyueliasonlycourses@gmail.com>
2025-10-24 21:26:19 +0300

    feat: remove SpeedInsights component from main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,10 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import LandingPage from "./LandingPage";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <LandingPage />
     <Analytics />
-    <SpeedInsights />
   </StrictMode>
 );
